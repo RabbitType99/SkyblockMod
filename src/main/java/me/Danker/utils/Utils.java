808bec27d4b852f679144ddce1b3ee9150e795f1
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-03-29 23:57:22 +0200

    draw Beacon Beams from NEU

--- src/main/java/me/Danker/utils/Utils.java
+++ src/main/java/me/Danker/utils/Utils.java
@@ -45,7 +45,9 @@ public class Utils {
 									  1065000, 1410000, 1900000, 2500000, 3300000, 4300000, 5600000, 7200000, 9200000, 12000000, 15000000,
 									  19000000, 24000000, 30000000, 38000000, 48000000, 60000000, 75000000, 93000000, 116250000};
 	static int[] expertiseKills = {50, 100, 250, 500, 1000, 2500, 5500, 10000, 15000};
-	
+
+	private static final ResourceLocation beaconBeam = new ResourceLocation("textures/entity/beacon_beam.png");
+
     public static int getItems(String item) {
     	Minecraft mc = Minecraft.getMinecraft();
     	EntityPlayer player = mc.thePlayer;
@@ -532,6 +534,120 @@ public class Utils {
 			default:
 				return null;
 		}
+	}
+
+	public static void drawWaypoint(BlockPos pos, Color color, float partialTicks) {
+		/**
+		 * Draw a NEU like Waypoint that has a beacon beam and a outline on the
+		 * target block.
+		 * Credit: https://github.com/bowser0000/SkyblockMod/pull/69,
+		 * 		   https://github.com/Moulberry/NotEnoughUpdates/
+		 */
+		if (pos==null) {
+			return;
+		}
+		Entity viewer = Minecraft.getMinecraft().getRenderViewEntity();
+		double viewerX = viewer.lastTickPosX + (viewer.posX - viewer.lastTickPosX) * partialTicks;
+		double viewerY = viewer.lastTickPosY + (viewer.posY - viewer.lastTickPosY) * partialTicks;
+		double viewerZ = viewer.lastTickPosZ + (viewer.posZ - viewer.lastTickPosZ) * partialTicks;
+
+		double x = pos.getX() - viewerX;
+		double y = pos.getY() - viewerY;
+		double z = pos.getZ() - viewerZ;
+		double distSq = x*x + y*y + z*z;
+
+		GlStateManager.disableDepth();
+		GlStateManager.disableCull();
+		GlStateManager.disableTexture2D();
+		System.out.println("Drew 3D Box");
+		if (distSq > 5*5) renderBeaconBeam(x, y, z, color.getRGB(), 1.0f, partialTicks);
+		Utils.draw3DBox(new AxisAlignedBB(pos, pos.add(1, 1, 1)), new Color(173, 216, 230).getRGB(), partialTicks);
+		GlStateManager.disableLighting();
+		GlStateManager.enableTexture2D();
+		GlStateManager.enableDepth();
+	}
+
+	public static void renderBeaconBeam(double x, double y, double z, int rgb, float alphaMult, float partialTicks) {
+		/**
+		 * Draw a beacon beam at the target location with the specified colour
+		 * Credit: https://github.com/Moulberry/NotEnoughUpdates/
+		 */
+		int height = 300;
+		int bottomOffset = 0;
+		int topOffset = bottomOffset + height;
+
+		Tessellator tessellator = Tessellator.getInstance();
+		WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+
+		Minecraft.getMinecraft().getTextureManager().bindTexture(beaconBeam);
+		GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, 10497.0F);
+		GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, 10497.0F);
+		GlStateManager.disableLighting();
+		GlStateManager.enableCull();
+		GlStateManager.enableTexture2D();
+		GlStateManager.tryBlendFuncSeparate(770, 1, 1, 0);
+		GlStateManager.enableBlend();
+		GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+
+		double time = Minecraft.getMinecraft().theWorld.getTotalWorldTime() + (double)partialTicks;
+		double d1 = MathHelper.func_181162_h(-time * 0.2D - (double)MathHelper.floor_double(-time * 0.1D));
+
+		float r = ((rgb >> 16) & 0xFF) / 255f;
+		float g = ((rgb >> 8) & 0xFF) / 255f;
+		float b = (rgb & 0xFF) / 255f;
+		double d2 = time * 0.025D * -1.5D;
+		double d4 = 0.5D + Math.cos(d2 + 2.356194490192345D) * 0.2D;
+		double d5 = 0.5D + Math.sin(d2 + 2.356194490192345D) * 0.2D;
+		double d6 = 0.5D + Math.cos(d2 + (Math.PI / 4D)) * 0.2D;
+		double d7 = 0.5D + Math.sin(d2 + (Math.PI / 4D)) * 0.2D;
+		double d8 = 0.5D + Math.cos(d2 + 3.9269908169872414D) * 0.2D;
+		double d9 = 0.5D + Math.sin(d2 + 3.9269908169872414D) * 0.2D;
+		double d10 = 0.5D + Math.cos(d2 + 5.497787143782138D) * 0.2D;
+		double d11 = 0.5D + Math.sin(d2 + 5.497787143782138D) * 0.2D;
+		double d14 = -1.0D + d1;
+		double d15 = (double)(height) * 2.5D + d14;
+		worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+		worldrenderer.pos(x + d4, y + topOffset, z + d5).tex(1.0D, d15).color(r, g, b, 1.0F*alphaMult).endVertex();
+		worldrenderer.pos(x + d4, y + bottomOffset, z + d5).tex(1.0D, d14).color(r, g, b, 1.0F).endVertex();
+		worldrenderer.pos(x + d6, y + bottomOffset, z + d7).tex(0.0D, d14).color(r, g, b, 1.0F).endVertex();
+		worldrenderer.pos(x + d6, y + topOffset, z + d7).tex(0.0D, d15).color(r, g, b, 1.0F*alphaMult).endVertex();
+		worldrenderer.pos(x + d10, y + topOffset, z + d11).tex(1.0D, d15).color(r, g, b, 1.0F*alphaMult).endVertex();
+		worldrenderer.pos(x + d10, y + bottomOffset, z + d11).tex(1.0D, d14).color(r, g, b, 1.0F).endVertex();
+		worldrenderer.pos(x + d8, y + bottomOffset, z + d9).tex(0.0D, d14).color(r, g, b, 1.0F).endVertex();
+		worldrenderer.pos(x + d8, y + topOffset, z + d9).tex(0.0D, d15).color(r, g, b, 1.0F*alphaMult).endVertex();
+		worldrenderer.pos(x + d6, y + topOffset, z + d7).tex(1.0D, d15).color(r, g, b, 1.0F*alphaMult).endVertex();
+		worldrenderer.pos(x + d6, y + bottomOffset, z + d7).tex(1.0D, d14).color(r, g, b, 1.0F).endVertex();
+		worldrenderer.pos(x + d10, y + bottomOffset, z + d11).tex(0.0D, d14).color(r, g, b, 1.0F).endVertex();
+		worldrenderer.pos(x + d10, y + topOffset, z + d11).tex(0.0D, d15).color(r, g, b, 1.0F*alphaMult).endVertex();
+		worldrenderer.pos(x + d8, y + topOffset, z + d9).tex(1.0D, d15).color(r, g, b, 1.0F*alphaMult).endVertex();
+		worldrenderer.pos(x + d8, y + bottomOffset, z + d9).tex(1.0D, d14).color(r, g, b, 1.0F).endVertex();
+		worldrenderer.pos(x + d4, y + bottomOffset, z + d5).tex(0.0D, d14).color(r, g, b, 1.0F).endVertex();
+		worldrenderer.pos(x + d4, y + topOffset, z + d5).tex(0.0D, d15).color(r, g, b, 1.0F*alphaMult).endVertex();
+		tessellator.draw();
+
+		GlStateManager.disableCull();
+		double d12 = -1.0D + d1;
+		double d13 = height + d12;
+
+		worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+		worldrenderer.pos(x + 0.2D, y + topOffset, z + 0.2D).tex(1.0D, d13).color(r, g, b, 0.25F*alphaMult).endVertex();
+		worldrenderer.pos(x + 0.2D, y + bottomOffset, z + 0.2D).tex(1.0D, d12).color(r, g, b, 0.25F).endVertex();
+		worldrenderer.pos(x + 0.8D, y + bottomOffset, z + 0.2D).tex(0.0D, d12).color(r, g, b, 0.25F).endVertex();
+		worldrenderer.pos(x + 0.8D, y + topOffset, z + 0.2D).tex(0.0D, d13).color(r, g, b, 0.25F*alphaMult).endVertex();
+		worldrenderer.pos(x + 0.8D, y + topOffset, z + 0.8D).tex(1.0D, d13).color(r, g, b, 0.25F*alphaMult).endVertex();
+		worldrenderer.pos(x + 0.8D, y + bottomOffset, z + 0.8D).tex(1.0D, d12).color(r, g, b, 0.25F).endVertex();
+		worldrenderer.pos(x + 0.2D, y + bottomOffset, z + 0.8D).tex(0.0D, d12).color(r, g, b, 0.25F).endVertex();
+		worldrenderer.pos(x + 0.2D, y + topOffset, z + 0.8D).tex(0.0D, d13).color(r, g, b, 0.25F*alphaMult).endVertex();
+		worldrenderer.pos(x + 0.8D, y + topOffset, z + 0.2D).tex(1.0D, d13).color(r, g, b, 0.25F*alphaMult).endVertex();
+		worldrenderer.pos(x + 0.8D, y + bottomOffset, z + 0.2D).tex(1.0D, d12).color(r, g, b, 0.25F).endVertex();
+		worldrenderer.pos(x + 0.8D, y + bottomOffset, z + 0.8D).tex(0.0D, d12).color(r, g, b, 0.25F).endVertex();
+		worldrenderer.pos(x + 0.8D, y + topOffset, z + 0.8D).tex(0.0D, d13).color(r, g, b, 0.25F*alphaMult).endVertex();
+		worldrenderer.pos(x + 0.2D, y + topOffset, z + 0.8D).tex(1.0D, d13).color(r, g, b, 0.25F*alphaMult).endVertex();
+		worldrenderer.pos(x + 0.2D, y + bottomOffset, z + 0.8D).tex(1.0D, d12).color(r, g, b, 0.25F).endVertex();
+		worldrenderer.pos(x + 0.2D, y + bottomOffset, z + 0.2D).tex(0.0D, d12).color(r, g, b, 0.25F).endVertex();
+		worldrenderer.pos(x + 0.2D, y + topOffset, z + 0.2D).tex(0.0D, d13).color(r, g, b, 0.25F*alphaMult).endVertex();
+		tessellator.draw();
+		System.out.println("Drew Beacon Beam");
 	}
 	
 }
